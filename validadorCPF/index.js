c2b644b1ec4445d4172696f9b01a532051b0f446
Gabriel Pinos Camargo <gabrielpinos03@gmail.com>
2020-11-04 17:34:09 -0300

    validador cpf criado por mim

---
+++ validadorCPF/index.js
@@ -0,0 +1,36 @@
+function cpfIsValid(cpf){
+
+    let cpfLimpo = cpf.replace(/\D+/g, '');
+   
+    let cpfArray = Array.from(cpfLimpo);
+    let cpfArrayLimpo = [...cpfArray];
+
+
+    cpfArray.splice(-2, 2);
+
+
+    for(let i = 0; i <= 1; i++){
+        let multiplicador = 10 + i;
+
+        let teste = cpfArray.reduce( (ac, valor) => {
+            ac += Number(valor) * multiplicador;
+            multiplicador--; 
+
+            return ac;
+        }, 0);        
+        
+        let verificador = String(11 - (teste % 11));
+
+        cpfArray.push(verificador);
+    }
+
+    
+    let intersecao = cpfArrayLimpo.filter((x,i) => cpfArray[i] == x);
+
+    return intersecao.length === 11 ? true : false;
+    
+}
+
+
+let cpf = '469.986.830-00';
+console.log(cpfIsValid(cpf));
