998e9f60eccb624b3f26e6638fd9fc064f3d3603
Gabriel Pinos Camargo <gabrielpinos03@gmail.com>
2022-04-11 18:56:19 -0300

    axios config

--- ReactProjects/react-base/src/pages/Login/index.js
+++ ReactProjects/react-base/src/pages/Login/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
 import { Container } from '../../styles/GlobalStyles';
 import { Title } from './styled';
 
+import axios from '../../services/axios';
+
 export default function Login() {
+  React.useEffect(() => {
+    async function getDate() {
+      const response = await axios.get('/alunos');
+      console.log(response.data);
+    }
+
+    getDate();
+  }, []);
   return (
     <Container>
       <Title>
