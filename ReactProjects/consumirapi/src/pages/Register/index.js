538e9b46de0ddea31ac9363faf98e6bad816f734
Gabriel Pinos Camargo <gabrielpinos03@gmail.com>
2022-04-18 20:50:17 -0300

    use api project

---
+++ ReactProjects/consumirapi/src/pages/Register/index.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
+import axios from '../../services/axios';
+
+export default function Login() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    let formErrors = false;
+
+    if (nome.length < 3 || nome.length > 255) {
+      formErrors = true;
+      toast.error('Nome deve ter entre 3 e 255 caracteres');
+    }
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('E-mail inválido');
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error('Senha deve ter entre 6 e 50 caracteres');
+    }
+
+    if (formErrors) return;
+
+    try {
+      const response = await axios.post('/users/', {
+        nome,
+        password,
+        email,
+      });
+
+      console.log(response);
+    } catch (e) {}
+  }
+
+  return (
+    <Container>
+      <h1>Crie sua conta</h1>
+      <Form onSubmit={handleSubmit}>
+        <label htmlFor="nome">
+          Nome:
+          <input
+            name="nome"
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeholder="Seu nome"
+          />
+        </label>
+        <label htmlFor="email">
+          E-mail:
+          <input
+            name="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Seu E-mail"
+          />
+        </label>
+        <label htmlFor="senha">
+          Senha:
+          <input
+            name="senha"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Sua senha"
+          />
+        </label>
+
+        <button type="submit">Criar minha conta</button>
+      </Form>
+    </Container>
+  );
+}
